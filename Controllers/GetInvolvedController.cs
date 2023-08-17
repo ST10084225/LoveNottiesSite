cbed2e16156a353f4607b86f194c24a95abe7140
ST10084795 <nathanxbelling@gmail.com>
2023-08-17 09:07:59 +0200

    StaticPages

---
+++ Controllers/GetInvolvedController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LoveNottiesV2.Controllers
+{
+    public class GetInvolvedController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
